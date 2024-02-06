6d794aa2bb49c36ee5fe07aed7c9bfdd5dd8a80d
ismailjosim <ismailjosim99@gmail.com>
2024-02-07 00:25:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './layouts/Header';
+
+
+const App = () => {
+  return (
+    <section className='w-full h-screen bg-zinc-900 text-white'>
+      <Header />
+    </section>
+  );
+};
+
+export default App;
